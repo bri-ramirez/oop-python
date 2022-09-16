140cb97265f68233cfb775b4291a1748d2f2a13d
bri-ramirez <briggitte.ramirez13@gmail.com>
2022-09-16 16:16:52 -0300

    cuenta bancaria

---
+++ cuenta_bancaria.py
@@ -0,0 +1,54 @@
+class CuentaBancaria:
+    todas_las_cuentas = []
+
+    def __init__(self,  balance = 0, tasa_interes = 0.01): 
+        self.tasa_interes = tasa_interes
+        self.balance = balance
+        CuentaBancaria.todas_las_cuentas.append(self)
+
+    def deposito(self, amount):
+        self.balance += amount
+        return self
+
+    def retiro(self, amount):
+        self.balance -= amount
+        return self
+    
+    def mostrar_info_cuenta(self):
+        print(self.balance)
+        return self
+
+    def generar_interes(self):
+        if self.balance > 0:
+            self.balance += self.balance * self.tasa_interes
+        return self
+
+    @classmethod
+    def imprimir_todas_las_cuentas(cls):
+        for cuenta in cls.todas_las_cuentas:
+            print(f"balance de cuenta: {cuenta.balance}")
+            # imprime la instancia como un diccionario
+            # print(cuenta.__dict__)
+
+
+cuenta1 = CuentaBancaria()
+cuenta2 = CuentaBancaria()
+
+cuenta1.deposito(1000) \
+    .deposito(9000) \
+    .deposito(400) \
+    .retiro(1000) \
+    .generar_interes() \
+    .mostrar_info_cuenta()
+
+cuenta2.deposito(2000) \
+    .deposito(600) \
+    .retiro(500) \
+    .retiro(200) \
+    .retiro(600) \
+    .retiro(200) \
+    .generar_interes() \
+    .mostrar_info_cuenta()
+
+CuentaBancaria.imprimir_todas_las_cuentas()
+
