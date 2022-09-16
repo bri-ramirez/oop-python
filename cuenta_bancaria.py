9698465d4371502a1354887e74b6cdc9f1f40315
bri-ramirez <briggitte.ramirez13@gmail.com>
2022-09-16 16:48:22 -0300

    asignacion cuenta bancaria

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -31,24 +31,3 @@ class CuentaBancaria:
             # print(cuenta.__dict__)
 
 
-cuenta1 = CuentaBancaria()
-cuenta2 = CuentaBancaria()
-
-cuenta1.deposito(1000) \
-    .deposito(9000) \
-    .deposito(400) \
-    .retiro(1000) \
-    .generar_interes() \
-    .mostrar_info_cuenta()
-
-cuenta2.deposito(2000) \
-    .deposito(600) \
-    .retiro(500) \
-    .retiro(200) \
-    .retiro(600) \
-    .retiro(200) \
-    .generar_interes() \
-    .mostrar_info_cuenta()
-
-CuentaBancaria.imprimir_todas_las_cuentas()
-
